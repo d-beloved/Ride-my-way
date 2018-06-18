574c39d6d9920e8b9458d9c01cf2f14942c0b570
d-beloved <morayodeji@gmail.com>
2018-06-18 18:41:17 +0100

    [Feature #158410384] Add an endpoint to create ride offer [(Delivers) #158410384]

---
+++ server/routes/routes.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import validateRequest from '../helpers/validation';
+import rideOfferController from '../controllers/rideOffer';
+
+// using router routes
+const router = express.Router();
+
+/** ****************** MY API ENDPOINTS *************** */
+
+// Welcome message route
+router.get('/', (req, res) => {
+  const rootMessage = {
+    message: 'Welcome to Ride-My-Way app! Your one stop place to get rides to your desired destination at reasonable prices',
+  };
+  res.status(200).json(rootMessage);
+});
+
+// Create a ride offer
+router.post(
+  '/rides',
+  validateRequest.trimBodyValues,
+  validateRequest.checkBodyContains('title', 'driverName', 'destination', 'deparTerminal', 'date', 'fee'),
+  validateRequest.confirmDate,
+  validateRequest.confirmFeeType,
+  rideOfferController.createRideOffer,
+);
+
+// 404 route
+router.all('*', (req, res) => {
+  res.status(404).send({ message: 'That route does not exist!' });
+});
+
+export default router;
