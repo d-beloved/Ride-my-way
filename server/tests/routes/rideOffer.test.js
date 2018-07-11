7e362dd07b01bf8047a1ea84ac790032ee7d70e5
d-beloved <morayodeji@gmail.com>
2018-07-11 05:04:14 +0100

    Chore - Add tests for ride_offers route

--- server/tests/routes/rideOffer.test.js
+++ server/tests/routes/rideOffer.test.js
@@ -1,216 +1,152 @@
-// import { expect } from 'chai';
-// import request from 'supertest';
-// import server from '../../app';
+import jwt from 'jsonwebtoken';
+import request from 'supertest';
+import { expect } from 'chai';
+import server from '../../app';
 
-// describe('The Ride Offer routes', () => {
-//   describe('Register a ride offer route', () => {
-//     // Registers a ride offer
-//     it('Should return 400 for missing required fields', (done) => {
-//       request(server)
-//         .post('/api/v1/rides')
-//         .send({
-//           title: 'Dave travels',
-//           driverName: 'Gulliver',
-//           destination: 'Los Angeles',
-//           depart: 'MMA 2 terminal',
-//           date: '',
-//           fee: 300000
-//         })
-//         .end((err, res) => {
-//           expect(res.status).to.equal(400);
-//           expect(res.body).to.be.an('object');
-//           expect(res.body.message).to.equal('date cannot be missing in the body!');
-//           done();
-//         });
-//     });
-//     it('Should return 409 if ride offer exists already', (done) => {
-//       request(server)
-//         .post('/api/v1/rides')
-//         .send({
-//           title: 'Dave travels',
-//           driverName: 'GUO transport',
-//           destination: 'Abuja, Lagoon',
-//           depart: 'MMA 2 terminal',
-//           date: '2018-07-25',
-//           fee: 300000
-//         })
-//         .end((err, res) => {
-//           expect(res.status).to.equal(409);
-//           expect(res.body).to.be.an('object');
-//           expect(res.body.message).to.equal('A Ride Offer with same driver Name and destination is found!');
-//           done();
-//         });
-//     });
-//     it('Should return 406 if an invalid date is entered', (done) => {
-//       request(server)
-//         .post('/api/v1/rides')
-//         .send({
-//           title: 'Dave travels',
-//           driverName: 'Gulliver',
-//           destination: 'Los Angeles',
-//           depart: 'MMA 2 terminal',
-//           date: '2018-06-20',
-//           fee: 300000
-//         })
-//         .end((err, res) => {
-//           expect(res.status).to.equal(406);
-//           expect(res.body).to.be.an('object');
-//           expect(res.body.message).to.equal('Please enter a valid future date in this format (YYYY-MM-DD)');
-//           done();
-//         });
-//     });
-//     it('Should return status 400 if a non integer is entered as fee', (done) => {
-//       request(server)
-//         .post('/api/v1/rides')
-//         .send({
-//           title: 'Dave travels',
-//           driverName: 'Gulliver',
-//           destination: 'Los Angeles',
-//           depart: 'MMA 2 terminal',
-//           date: '2018-07-25',
-//           fee: '300000'
-//         })
-//         .end((err, res) => {
-//           expect(res.status).to.equal(400);
-//           expect(res.body).to.be.an('object');
-//           expect(res.body.message).to.equal('Please enter a valid amount for the ride');
-//           done();
-//         });
-//     });
-//     it('Should return status 201 when ride offer is created', (done) => {
-//       request(server)
-//         .post('/api/v1/rides')
-//         .send({
-//           title: 'Dave travels',
-//           driverName: 'Gulliver',
-//           destination: 'Los Angeles',
-//           depart: 'MMA 2 terminal',
-//           date: '2018-07-25',
-//           fee: 300000
-//         })
-//         .end((err, res) => {
-//           expect(res.status).to.equal(201);
-//           expect(res.body).to.be.an('object');
-//           expect(res.body.message).to.equal('Ride offer created successfully');
-//           done();
-//         });
-//     });
-//   });
+const wrongtoken = 'wrong secret';
+const authToken1 = jwt.sign(1, process.env.JWT_SECRET);
+const token1 = `Bearer ${authToken1}`;
 
-//   describe('Get all ride offers route', () => {
-//     // Returns result after searching for offers by destination
-//     it('Should return 404 if there is no ride with the queried destination', (done) => {
-//       request(server)
-//         .get('/api/v1/rides?destination=Dapchi')
-//         .end((err, res) => {
-//           expect(res.status).to.equal(404);
-//           expect(res.body).to.be.an('object');
-//           expect(res.body.message).to.equal('There is no Ride offer with the specified destination');
-//           done();
-//         });
-//     });
-//     it('Should return 200 if rides to the queried destination is found', (done) => {
-//       request(server)
-//         .get('/api/v1/rides?destination=lagos')
-//         .end((err, res) => {
-//           expect(res.status).to.equal(200);
-//           done();
-//         });
-//     });
-//     it('Should return 200 if ride offers were found or not found', (done) => {
-//       request(server)
-//         .get('/api/v1/rides')
-//         .end((err, res) => {
-//           expect(res.status).to.equal(200);
-//           expect(res.body).to.be.an('object');
-//           done();
-//         });
-//     });
-//   });
+describe('The Ride Offer routes', () => {
+  describe('A user is authenticated with a token', () => {
+    it('Should return 401 for an empty token', (done) => {
+      request(server)
+        .post('/api/v1/users/rides')
+        .set({ authorization: '' })
+        .send({
+          message: 'Dave travels',
+          destination: 'Los Angeles',
+          departurelocation: 'MMA 2 terminal',
+          date: '27-07-2018'
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(401);
+          expect(res.body).to.be.an('object');
+          expect(res.body.message).to.equal('token is required!');
+          done();
+        });
+    });
+    it('Should return 401 for a wrong token', (done) => {
+      request(server)
+        .post('/api/v1/users/rides')
+        .set({ authorization: wrongtoken })
+        .send({
+          message: 'Dave travels',
+          destination: 'Los Angeles',
+          departurelocation: 'MMA 2 terminal',
+          date: '27-07-2018'
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(401);
+          expect(res.body).to.be.an('object');
+          expect(res.body.message).to.equal('Authentication failed! Token is Invalid or expired. Please Login again');
+          done();
+        });
+    });
+  });
 
-//   describe('Get one ride offer route', () => {
-//     it('Should return 200 if ride offer was found', (done) => {
-//       request(server)
-//         .get('/api/v1/rides/1')
-//         .end((err, res) => {
-//           expect(res.status).to.equal(200);
-//           expect(res.body).to.be.an('object');
-//           done();
-//         });
-//     });
-//     it('Should return 404 if ride offer was not found', (done) => {
-//       request(server)
-//         .get('/api/v1/rides/0')
-//         .end((err, res) => {
-//           expect(res.status).to.equal(404);
-//           expect(res.body).to.be.an('object');
-//           expect(res.body.message).to.equal('Ride Offer not found!');
-//           done();
-//         });
-//     });
-//   });
+  describe('Register a ride offer route', () => {
+    // Registers a ride offer
+    it('Should return 400 for missing required fields', (done) => {
+      request(server)
+        .post('/api/v1/users/rides')
+        .set({ authorization: token1 })
+        .send({
+          message: 'Dave travels',
+          destination: 'Los Angeles',
+          departurelocation: 'MMA 2 terminal',
+          date: ' '
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(400);
+          expect(res.body).to.be.an('object');
+          expect(res.body.message).to.equal('date cannot be missing in the body!');
+          done();
+        });
+    });
+    it('Should return 400 for entering a wrong type where there should be string', (done) => {
+      request(server)
+        .post('/api/v1/users/rides')
+        .set({ authorization: token1 })
+        .send({
+          message: 'Dave travels',
+          destination: 112,
+          departurelocation: 'MMA 2 terminal',
+          date: '27-07-2018'
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(400);
+          expect(res.body).to.be.an('object');
+          expect(res.body.message).to.equal('destination must be a string value!');
+          done();
+        });
+    });
+    it('Should return status 201 when ride offer is created', (done) => {
+      request(server)
+        .post('/api/v1/users/rides')
+        .set({ authorization: token1 })
+        .send({
+          message: 'Dave travels',
+          destination: 'Los Angeles',
+          departurelocation: 'MMA 2 terminal',
+          date: '27-07-2018'
+        })
+        .end((err, res) => {
+          expect(res.status).to.equal(201);
+          expect(res.body).to.be.an('object');
+          expect(res.body.message).to.equal('Your ride offer is created successfully');
+          done();
+        });
+    });
+  });
 
-//   describe('Delete a ride offer', () => {
-//     /* Delete a Ride offer */
-//     it('Should return 404 if ride offer not found', (done) => {
-//       request(server)
-//         .delete('/api/v1/rides/0')
-//         .end((err, res) => {
-//           expect(res.status).to.equal(404);
-//           expect(res.body).to.be.an('object');
-//           expect(res.body.message).to.equal('Ride Offer not found or deleted already');
-//           done();
-//         });
-//     });
-//     it('Should return 200 if Ride offer was deleted successfully', (done) => {
-//       request(server)
-//         .delete('/api/v1/rides/2')
-//         .end((err, res) => {
-//           expect(res.status).to.equal(200);
-//           expect(res.body).to.be.an('object');
-//           expect(res.body.message).to.equal('Ride Offer deleted successfully');
-//           done();
-//         });
-//     });
-//   });
+  describe('Get all ride offers route', () => {
+    // Returns result after searching for offers by destination
+    it('Should return 200 if ride offers are in the app', (done) => {
+      request(server)
+        .get('/api/v1/rides')
+        .end((err, res) => {
+          expect(res.status).to.equal(200);
+          expect(res.body).to.be.an('object');
+          expect(res.body.message).to.equal('These are the ride offers we have');
+          done();
+        });
+    });
+  });
 
-//   describe('Modify a Ride offer', () => {
-//     /* Modify a ride offer */
-//     it('Should return 404 if ride offer not found', (done) => {
-//       request(server)
-//         .put('/api/v1/rides/0')
-//         .send({
-//           title: 'Dave travels',
-//           driverName: 'Gullion',
-//           destination: 'Los Angeles',
-//           depart: 'MMA 2 terminal',
-//           date: '2018-07-27',
-//           fee: '300000'
-//         })
-//         .end((err, res) => {
-//           expect(res.status).to.equal(404);
-//           expect(res.body).to.be.an('object');
-//           expect(res.body.message).to.equal('Ride Offer not found!');
-//           done();
-//         });
-//     });
-//     it('Should return 200 if ride offer was edited successfully', (done) => {
-//       request(server)
-//         .put('/api/v1/rides/3')
-//         .send({
-//           title: 'Dave travels',
-//           driverName: 'Gullion',
-//           destination: 'Los Angeles',
-//           depart: 'MMA 2 terminal',
-//           date: '2018-07-27',
-//           fee: '300000'
-//         })
-//         .end((err, res) => {
-//           expect(res.status).to.equal(200);
-//           expect(res.body).to.be.an('object');
-//           expect(res.body.message).to.equal('Ride Offer updated successfully!');
-//           done();
-//         });
-//     });
-//   });
-// });
+  describe('Get one ride offer route', () => {
+    it('Should return 400 if an invalid ID is entered in the parameter', (done) => {
+      request(server)
+        .get('/api/v1/rides/a')
+        .set({ authorization: token1 })
+        .end((err, res) => {
+          expect(res.status).to.equal(400);
+          expect(res.body).to.be.an('object');
+          expect(res.body.message).to.equal('You passed an invalid ID');
+          done();
+        });
+    });
+    it('Should return 200 if ride offer was found', (done) => {
+      request(server)
+        .get('/api/v1/rides/1')
+        .set({ authorization: token1 })
+        .end((err, res) => {
+          expect(res.status).to.equal(200);
+          expect(res.body).to.be.an('object');
+          expect(res.body.message).to.equal('Ride offer delivered');
+          done();
+        });
+    });
+    it('Should return 404 if ride offer was not found', (done) => {
+      request(server)
+        .get('/api/v1/rides/7')
+        .set({ authorization: token1 })
+        .end((err, res) => {
+          expect(res.status).to.equal(404);
+          expect(res.body).to.be.an('object');
+          expect(res.body.message).to.equal('Ride Offer not found!');
+          done();
+        });
+    });
+  });
+});
