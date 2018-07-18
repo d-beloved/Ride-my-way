dc2980211ddb0a9a089680432555ef53907dcb5e
d-beloved <morayodeji@gmail.com>
2018-07-18 21:22:38 +0100

    BugFixes - Fixed the inertion test

--- server/tests/seeder.js
+++ server/tests/seeder.js
@@ -6,21 +6,15 @@ const hashedPassword = bcrypt.hashSync('ispassword', 10);
 
 const sql = 'INSERT INTO aUsers (firstname, lastname, phoneno, username, email, password) VALUES($1,$2,$3,$4,$5,$6)';
 
-const sql2 = 'INSERT INTO bRide_offers (message, destination, departurelocation, date, userId) VALUES($1,$2,$3,$4,$5)';
-
 const data1 = ['ayodeji', 'moronkeji', 70563224566, 'david', 'morayodeji@gmail.com', hashedPassword];
 
 const data2 = ['gwen', 'gbenga', 90975434567, 'deji', 'davewritchie@gmail.com', hashedPassword];
 
-const data3 = ['Let\'s take a trip to the Bahamas', 'Bahamas', 'Lagos', '2018-07-27', 1];
-
 const client = new Client(connectionString);
 const client2 = new Client(connectionString);
-const client3 = new Client(connectionString);
 
 client.connect();
 client2.connect();
-client3.connect();
 client.query(sql, data1, (err) => {
   if (err) {
     client.end();
@@ -40,6 +34,13 @@ client2.query(sql, data2, (err) => {
     console.log('user inserted');
   }
 });
+
+const sql2 = 'INSERT INTO bRide_offers (message, destination, departurelocation, date, userId) VALUES($1,$2,$3,$4,$5)';
+
+const data3 = ['Let\'s take a trip to the Bahamas', 'Bahamas', 'Lagos', '2018-07-27', '1'];
+
+const client3 = new Client(connectionString);
+client3.connect();
 
 client3.query(sql2, data3, (err) => {
   if (err) {
