f58cc89d1c23a0c9bb4987ccbfedc78d15938e1f
d-beloved <morayodeji@gmail.com>
2018-07-18 22:42:18 +0100

    Chore - update the UI in accordance to gh-pages update

--- client/js/main.js
+++ client/js/main.js
@@ -12,9 +12,9 @@ const checkform = () => {
   let form1 = document.getElementById('my-form');
   if(form1.password.value != form1.confirmPassword.value)
   {
-    alert("Passwords must be the same");
-    form1.password.focus();
-    return false;
+      alert("Passwords must be the same");
+      form1.password.focus();
+      return false;
   }
   return true;
 }
