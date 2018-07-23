925be1ec64064ff686bb4c433e69c05eaacfc9b5
d-beloved <morayodeji@gmail.com>
2018-07-23 04:18:25 +0100

    Implemented the logout function

--- client/js/main.js
+++ client/js/main.js
@@ -1,9 +1,17 @@
-let navToggler = document.getElementById('navbar-toggler');
-let navCollapse = document.getElementById('navbar-collapse');
+/* eslint-disable no-undef, no-unused-vars */
+const logout = document.getElementById('logout');
+const navToggler = document.getElementById('navbar-toggler');
+const navCollapse = document.getElementById('navbar-collapse');
 
 const toggleNav = () => {
   console.log('clicked');
   navCollapse.classList.toggle('navbar-show');
 }
 
+const logoutUser = () => {
+  localStorage.removeItem('token');
+  localStorage.removeItem('user');
+};
+
 navToggler.addEventListener('click', toggleNav);
+logout.addEventListener('click', logoutUser);
