ab59f719a2647cf683c6342d97db69329bae993c
d-beloved <morayodeji@gmail.com>
2018-07-23 10:16:13 +0100

    [Feature #159197825]consume the create ride API [(Finishes) #159197825]

--- client/js/createRide.js
+++ client/js/createRide.js
@@ -1,100 +1,41 @@
 /* eslint-disable no-undef */
+/* eslint-disable no-alert */
 const alertLog = document.getElementById('alertLog');
-const alertMessage = document.getElementById('alertMessage');
-const errorMessage = document.querySelectorAll('.error');
-const user = document.getElementById('user');
+const successMessage = document.getElementById('alertMessage');
+const createRide = document.getElementById('createRide');
+const route = '/api/v1/users/rides';
+const token = `Bearer ${localStorage.getItem('token')}`;
 
-const { requestForm } = document.forms;
-const {
-  type, category, requestItem, description,
-} = requestForm.elements;
-
-user.innerText = localStorage.getItem('user');
-
-const displayErrorMessages = (error) => {
-  if (error.type) {
-    const [typeError] = error.type;
-    errorMessage[0].classList.add('display-error');
-    errorMessage[0].innerText = typeError;
-  }
-
-  if (error.category) {
-    const [categoryError] = error.category;
-    errorMessage[1].classList.add('display-error');
-    errorMessage[1].innerText = categoryError;
-  }
-
-  if (error.item) {
-    const [itemError] = error.item;
-    errorMessage[2].classList.add('display-error');
-    errorMessage[2].innerText = itemError;
-  }
-
-  if (error.description) {
-    const [descriptionError] = error.description;
-    errorMessage[3].classList.add('display-error');
-    errorMessage[3].innerText = descriptionError;
-  }
-};
-
-const clearErrorMeassage = (e) => {
-  e.target.parentElement.nextElementSibling.innerText = '';
-  e.target.parentElement.nextElementSibling.classList.remove('display-error');
-};
-
-const createRequest = (e) => {
-  e.preventDefault();
-  const requestDetails = {
-    type: type.value,
-    category: category.value,
-    item: requestItem.value,
-    description: description.value,
+const createRideOffer = (evt) => {
+  evt.preventDefault();
+  const headers = new Headers({
+    'content-type': 'application/json',
+    authorization: token
+  });
+  const rideDetails = {
+    message: createRide.message.value,
+    destination: createRide.destination.value,
+    departurelocation: createRide.departureLocation.value,
+    date: createRide.date.value
   };
-
-  const token = localStorage.getItem('token');
-
-  const option = {
+  
+  fetch(route, {
     method: 'POST',
-    headers: {
-      'Content-type': 'application/json',
-      'x-access-token': token,
-    },
-    body: JSON.stringify(requestDetails),
-  };
-
-  fetch(
-    'https://maintenance-tracker-stv.herokuapp.com/api/v1/users/requests',
-    option,
-  ).then((response) => {
-    if (response.status === 409) {
-      alertLog.style.display = 'block';
-      alertLog.classList.add('fail');
-      alertMessage.innerText = 'request already exist';
-      clearMessage();
-    }
-
-    return response.json();
+    body: JSON.stringify(rideDetails),
+    headers,
   })
-    .then((response) => {
-      if (response.status === 'success') {
-        alertLog.style.display = 'block';
-        alertLog.classList.remove('fail');
-        alertLog.classList.add('success');
-        alertMessage.innerText = response.message;
-        clearMessage();
-        window.location.href =
-        'https://maintenance-tracker-stv.herokuapp.com/user-view-requests.html';
+    .then(res => Promise.all([res.json(), res]))
+    .then(([data, res]) => {
+      if (!res.ok) {
+        return alert(JSON.stringify(data));
       }
-
-      if (response.status === 'fail') {
-        displayErrorMessages(response.data.errors);
-      }
+      alertLog.style.display = 'block';
+      alertLog.classList.add('success');
+      successMessage.innerText = 'Ride Created Successfully!';
+      clearMessage();
+      window.location.href = '/profile.html';
     })
-    .catch(err => err.message);
+    .catch(error => alert(error.message));
 };
 
-type.addEventListener('focus', clearErrorMeassage);
-category.addEventListener('focus', clearErrorMeassage);
-requestItem.addEventListener('focus', clearErrorMeassage);
-description.addEventListener('focus', clearErrorMeassage);
-requestForm.addEventListener('submit', createRequest);
+createRide.addEventListener('submit', createRideOffer);
--- client/js/main.js
+++ client/js/main.js
@@ -8,6 +8,14 @@ const toggleNav = () => {
   navCollapse.classList.toggle('navbar-show');
 }
 
+const clearMessage = () => {
+  window.setTimeout(() => {
+    alertLog.classList.remove('fail');
+    alertLog.classList.remove('success');
+    alertLog.style.display = 'none';
+  }, 4000);
+};
+
 const logoutUser = () => {
   localStorage.removeItem('token');
   localStorage.removeItem('user');
